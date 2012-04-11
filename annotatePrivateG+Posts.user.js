82969ed2ed2027017fc332f995fd163533cd89fa
Pax Unix <paxunix@gmail.com>
2012-04-10 21:26:14 -0700

    Indicate activity when checking audience
    - rather than changing the background colour, change the audience text
      and reset or modify it when the request finishes

--- annotatePrivateG+Posts.user.js
+++ annotatePrivateG+Posts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Mark 2-person posts on Google+ as "Private"
 // @namespace       halpenny
-// @description     If a Google+ post is shared with you and one other person, change the "Limited" audience text to "Private".  The audience popup will still work as before.  This script is very fragile and can be easily broken whenever Google updated Google+.  Caveat emptor.
+// @description     If a Google+ post is shared with you and one other person, change the "Limited" audience text to "Private".  The audience popup will still work as before.  This script can be easily broken whenever Google updated Google+.  Caveat emptor.
 // @include         https://plus.google.com/*
 // @version         0.1
 // ==/UserScript==
@@ -45,19 +45,22 @@ jQuery('div#contentPane').on("mouseenter", 'div[id^=update-]', function(e) {
     // it still appears that there are only two users in the audience data.
     // Return if the share is Public, rather than incorrectly marking it as
     // Private.
-    if (audience.text() === "Public")
+    var prevText = audience.text();
+    if (prevText === "Public")
         return;
 
+    // So the user has some indication that we're doing something, change
+    // the audience text.
+    audience.text("Checking...");
+
     var postId = post.id.match(/update-(.+)/)[1];
-    var req = jQuery.ajax("https://plus.google.com/u/0/_/stream/getaudience/", {
+    jQuery.ajax("https://plus.google.com/u/0/_/stream/getaudience/", {
         dataType: "text",
         data: {
             id: postId,
             buzz: "false",
         }
-    });
-
-    req.done(function(text) {
+    }).done(function (text) {
 
         // The response isn't true JSON.  Diddle it so we can get it into JSON
         // form and pull out the data we need.
@@ -72,10 +75,15 @@ jQuery('div#contentPane').on("mouseenter", 'div[id^=update-]', function(e) {
         var userData = JSON.parse(text)[0][2];
 
         // Only care if it was shared between 2 users
-        // XXX:  this also passes if the post was Public and shared only with
-        // one other person.
         if (userData.length == 2)
             audience[0].innerText = "Private";
+        else
+            audience[0].innerText = prevText;
+    }).fail(function (text) {
+        // For now, just log a message saying there was a problem.
+        console.log("Failed request to Google+ for post " + postId);
+
+        audience[0].innerText = prevText;
     });
 
 }); // jQuery.one
